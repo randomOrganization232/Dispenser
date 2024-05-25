a9693d61708a9bdff423285331583463cd311122
Adept <adeptiumion@gmail.com>
2024-05-25 18:11:40 +0700

    first commit

---
+++ src/main/java/com/dispenser/DispenserApplication.java
@@ -0,0 +1,13 @@
+package com.dispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispenserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dispenser/DispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
